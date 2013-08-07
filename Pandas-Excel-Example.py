9d4acb89d656f2759f75c2cb4668d08644311a72
Kevin Buchs <buchs.kevin@mayo.edu>
2013-08-07 15:07:24 -0500

    initial population

---
+++ Pandas-Excel-Example.py
@@ -0,0 +1,30 @@
+from pandas.io.parsers import ExcelWriter
+from pandas.io.parsers import ExcelFile
+
+xls = ExcelFile('Pandas-Example.xlsx')
+df=xls.parse('Sheet1')  # load the sheet into the Pandas Dataframe structure
+
+print("The list of row indicies")
+print(df.index)
+print("The column headings")
+print(df.columns)
+
+print("The 'Patient' column information:")
+print(df['Patient'])
+
+# print each row of the patient column
+for i in df.index:
+    print(df['Patient'][i])
+
+# compute a new column as the product of two other columns
+for i in df.index:
+    df['BP*SO2'][i] = df['BP'][i] * df['SO2'][i]
+
+print("results of column multiplication:")
+print(df['BP*SO2'])
+
+# write the dataframe back out with the new column data included
+writer = ExcelWriter('Pandas-Example-Out.xlsx')
+df.to_excel(writer,'Sheet1',index=False)
+writer.save()
+
